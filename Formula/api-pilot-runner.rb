d9be2c7f58a70ad91f93abc16bebe41eb2e00b93
faizalfakhri0001 <faizalfakhri0001@gmail.com>
2026-08-02 12:22:48 +0700

    chore(formula): publish runner 1.3.4

--- Formula/api-pilot-runner.rb
+++ Formula/api-pilot-runner.rb
@@ -1,13 +1,13 @@
 class ApiPilotRunner < Formula
   desc "Local runner agent for API Pilot"
   homepage "https://github.com/faizalfakhri0001/antasend-runner"
-  version "1.3.3"
+  version "1.3.4"
   if Hardware::CPU.arm?
-    url "https://github.com/faizalfakhri0001/antasend-runner/releases/download/api-pilot-runner-v1.3.3/api-pilot-runner-mac-arm64.tar.gz"
-    sha256 "8f12f9cf1e05d057e81f067c3f7027559ac375a514b869b2bf535f68571b8c30"
+    url "https://github.com/faizalfakhri0001/antasend-runner/releases/download/api-pilot-runner-v1.3.4/api-pilot-runner-mac-arm64.tar.gz"
+    sha256 "085c1929c2892c64c046a7ece6a8d9d44668089009185dca27434b30040403d5"
   else
-    url "https://github.com/faizalfakhri0001/antasend-runner/releases/download/api-pilot-runner-v1.3.3/api-pilot-runner-mac-amd64.tar.gz"
-    sha256 "99e7ecba24b8c793190458e54c936f8a0e766d8cb0108c17117b6f8b0d7933e2"
+    url "https://github.com/faizalfakhri0001/antasend-runner/releases/download/api-pilot-runner-v1.3.4/api-pilot-runner-mac-amd64.tar.gz"
+    sha256 "db835d56e9349d4f735e4d7370ec2031a239c1dc7f639c63a91c2fa49a3c661d"
   end
 
   def install
