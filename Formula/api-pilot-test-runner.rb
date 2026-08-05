e72e5922c81360f7b70e2271577ea0fa99972185
faizalfakhri0001 <64732731+faizalfakhri0001@users.noreply.github.com>
2026-08-05 09:32:05 +0700

    chore(release): publish api-pilot-test-runner v2.5.2 formula

--- Formula/api-pilot-test-runner.rb
+++ Formula/api-pilot-test-runner.rb
@@ -1,13 +1,13 @@
 class ApiPilotTestRunner < Formula
   desc "Local TestPilot browser runner for API Pilot"
   homepage "https://github.com/faizalfakhri0001/antasend-runner"
-  version "2.5.1"
+  version "2.5.2"
   if Hardware::CPU.arm?
-    url "https://github.com/faizalfakhri0001/antasend-runner/releases/download/api-pilot-test-runner-v2.5.1/api-pilot-test-runner-mac-arm64.tar.gz"
-    sha256 "a5250665ad627243d429f08c097cd34aa6e67f85c809b58ac2a9c0975d219598"
+    url "https://github.com/faizalfakhri0001/antasend-runner/releases/download/api-pilot-test-runner-v2.5.2/api-pilot-test-runner-mac-arm64.tar.gz"
+    sha256 "879b546655869a0c1d2df3a5f2a00e6c9c8241847c4d7f80d8be1a5c2b300309"
   else
-    url "https://github.com/faizalfakhri0001/antasend-runner/releases/download/api-pilot-test-runner-v2.5.1/api-pilot-test-runner-mac-amd64.tar.gz"
-    sha256 "a20daaee3d82d9d2acb8c79940f52a1ff87dd1da191f02d8dab41c5cadbbe07c"
+    url "https://github.com/faizalfakhri0001/antasend-runner/releases/download/api-pilot-test-runner-v2.5.2/api-pilot-test-runner-mac-amd64.tar.gz"
+    sha256 "a411748f5ac333828f22535217293ff8bb7916230077c20ad408f84159da3e07"
   end
 
   depends_on "node"
